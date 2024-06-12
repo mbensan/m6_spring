7a80ca4a27b4861865d214631f8c10a2202defdd
Matias <mbensan@gmail.com>
2024-06-12 09:32:52 -0400

    Se añaden modelos para el mensaje

--- el_muro/src/main/java/sust/el_muro/controllers/MuroController.java
+++ el_muro/src/main/java/sust/el_muro/controllers/MuroController.java
@@ -1,12 +1,19 @@
 package sust.el_muro.controllers;
 
+import org.aspectj.bridge.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import sust.el_muro.models.MessageRepository;
+
 @Controller
 public class MuroController {
+
+  @Autowired
+  MessageRepository messageRepo;
 
   @GetMapping("/")
   public ModelAndView muroScreen() {
---
+++ el_muro/src/main/java/sust/el_muro/models/MessageRepository.java
@@ -0,0 +1,7 @@
+package sust.el_muro.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+}

9f98d916468478801fe208915f598703648874da
Matias <mbensan@gmail.com>
2024-06-19 13:57:36 -0400

    Se agrega RestController

---
+++ muro/src/main/java/sust/muro/controllers/CommentRestController.java
@@ -0,0 +1,31 @@
+package sust.muro.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sust.muro.daos.CommentsDao;
+import sust.muro.models.Comment;
+import sust.muro.models.CommentRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentRestController {
+
+  @Autowired
+  CommentsDao commentDao;
+
+  @GetMapping("/{message_id}")
+  public List<Comment> getAll(@PathVariable Long message_id) {
+    List<Comment> comments = commentDao.getCommentsFromMessage(message_id);
+
+    return comments;
+  }
+
+}
--- muro/src/main/java/sust/muro/models/Comment.java
+++ muro/src/main/java/sust/muro/models/Comment.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -43,11 +44,13 @@ public class Comment {
   @DateTimeFormat(pattern = "yyyy-MM-dd")
   Date updated;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "message_id", referencedColumnName = "id")
   Message message;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", referencedColumnName = "id")
   User user;
+
+  String creatorsName;
 }
